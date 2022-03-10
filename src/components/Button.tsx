183570b28b14a2843f61749186bf41762acd617c
bumnum <test@example.com>
2022-03-10 21:53:17 +0100

    fix: minor adjustments to make it work better on smaller vertical screens

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -8,7 +8,7 @@ export const Button: FC<Props> = ({ children, onClick }) => {
   return (
     <button
       className={
-        "w-full xs:w-auto text-white uppercase bg-black hover:bg-gray-800 font-black px-8 py-3 mb-6 rounded flex items-center justify-center "
+        "w-full xs:w-auto text-white uppercase bg-black hover:bg-gray-800 font-black px-8 py-3 rounded flex items-center justify-center "
       }
       onClick={onClick}
     >
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import { Button } from "./Button";
 
 export const Header = () => {
   return (
-    <header className="fixed inset-x-0 top-0 p-5">
+    <header className="sm:fixed inset-x-0 top-0 p-5">
       <div className="flex justify-end">
         <Button onClick={() => {}}>Connect wallet</Button>
       </div>
